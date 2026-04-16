2d9ca36366ad8d440f1f71c798aad940aacd2991
mtriana14 <melanicapps@gmail.com>
2026-04-16 01:12:14 -0400

    f

--- app/config/db.py
+++ app/config/db.py
@@ -5,11 +5,11 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 def init_db(app):
+    # Añadimos el puerto después del host separado por dos puntos
     app.config['SQLALCHEMY_DATABASE_URI'] = (
         f"mysql+pymysql://{app.config['DB_USER']}:{app.config['DB_PASSWORD']}"
-        f"@{app.config['DB_HOST']}/{app.config['DB_NAME']}"
+        f"@{app.config['DB_HOST']}:{app.config['DB_PORT']}/{app.config['DB_NAME']}"
     )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
-    migrate.init_app(app, db) 
- 
+    migrate.init_app(app, db)
