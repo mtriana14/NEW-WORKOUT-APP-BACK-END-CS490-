909a672a9629274cd6e10b0d4e2a0517f9805c55
mtriana14 <melanicapps@gmail.com>
2026-04-16 01:43:34 -0400

    dd

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app import create_app
 from app.models import User, Coach, CoachAvailability, ClientRequest, Exercise, Notification, Payment, CoachRegistration, CoachManagement
 import os
 app = create_app()
-
+ 
 with app.app_context():
     from app.seeders.run_seeders import run_all_seeders
     #run_all_seeders()
