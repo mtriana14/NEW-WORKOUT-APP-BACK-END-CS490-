e42ba22c026869c3e90c3b9c818ad65760f42ca7
mtriana14 <melanicapps@gmail.com>
2026-04-16 01:38:40 -0400

    dd

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import create_app
 from app.models import User, Coach, CoachAvailability, ClientRequest, Exercise, Notification, Payment, CoachRegistration, CoachManagement
-
+import os
 app = create_app()
 
 with app.app_context():
@@ -8,4 +8,5 @@ with app.app_context():
     #run_all_seeders()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
