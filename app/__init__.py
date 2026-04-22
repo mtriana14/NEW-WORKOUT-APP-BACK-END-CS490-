e8eb9ed4efe357d84c3101429ae5cfe91e3a2c83
gnkev <kevguz2504@gmail.com>
2026-04-22 03:47:56 -0400

    Implemented workout plan assignment to plans, allowing for them to be deleted from a plan or workouts deleted as well from a plan.

--- app/__init__.py
+++ app/__init__.py
@@ -78,6 +78,8 @@ def create_app():
     app.register_blueprint(oauth_bp, url_prefix='/api')
     from app.routes.forgot_pass_routes import forgot_pass_bp
     app.register_blueprint(forgot_pass_bp, url_prefix='/api')
+    from app.routes.workout_calendar_routes import workout_calendar_bp
+    app.register_blueprint(workout_calendar_bp, url_prefix='/api')
 
 
 
